ae909989d8502959cbcb49e9047681b3964a70d5
Kharin-Ilya <102606492+Kharin-Ilya@users.noreply.github.com>
2022-04-19 22:01:16 +0400

    Delete task4.py

--- task4.py
+++
@@ -1,5 +0,0 @@
-with open(input(), encoding='utf-8')  as file:
-  
-    my_list = list(map(int, file.readlines()))
-mid = sorted( my_list)[len(my_list) // 2]
-print(sum(abs(i - mid) for i in my_list))
